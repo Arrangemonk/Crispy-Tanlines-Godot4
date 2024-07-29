d76729b217e0b44321a2297de8a156a3af4fb414
Arrangemonk <34814431+Arrangemonk@users.noreply.github.com>
2024-07-29 07:22:00 +0200

    init

---
+++ Code/Water.gd
@@ -0,0 +1,11 @@
+extends MeshInstance3D
+var timer : float
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	timer = 0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
